cc9ebefe8443cc8ca431a3198bf04fbac683536e
George-IRR <196656380+George-IRR@users.noreply.github.com>
2026-06-07 22:25:17 +0300

    refactor(adxl): optimize scaling logic
    - Define LIMIT_30_DEG at 128 to represent a ±30-degree tilt limit
    (0.5g).
    - Replace offset of 200 with 128 to match the new dynamic range.

--- Etapa_2/adxl_level.v
+++ Etapa_2/adxl_level.v
@@ -8,19 +8,19 @@ module adxl_level #(
 	 output 		  [$clog2(DISP_Y)-1:0] row_o	  
 );
 
+localparam LIMIT_30_DEG = 16'sd128;
+
 wire signed [15:0] raw_x_position;
 wire signed [15:0] raw_y_position;
 
-
-
-assign raw_x_position = ((data_xi + 200) * DISP_X) / 400;
+assign raw_x_position = ((data_xi + LIMIT_30_DEG) * DISP_X) >>> 8;
 
 assign column_o = (raw_x_position < 0)  ? 0 		  			   : 
                   (raw_x_position >= DISP_X) ? (DISP_X - 1) : 
                    raw_x_position[$clog2(DISP_X)-1:0]      	; 
-						 
 
-assign raw_y_position = ((data_yi + 200) * DISP_Y) / 400;
+
+assign raw_y_position = ((data_yi + LIMIT_30_DEG) * DISP_Y) >>> 8;
 
 assign row_o = (raw_y_position < 0)  ? 0 		  			   : 
                   (raw_y_position >= DISP_Y) ? (DISP_Y - 1) : 
